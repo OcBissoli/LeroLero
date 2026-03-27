62dc899621c19520cd7a995aa21a9f39ec870912
OcBissoli <pobissoli@gmail.com>
2026-03-27 11:46:28 -0300

    ENGLISH do Sarney

--- lerolero.py
+++ lerolero.py
@@ -4,4 +4,10 @@ parte_1 = ["A", "B", "C"]
 parte_2 = ["D", "E", "F"]
 parte_3 = ["G", "H", "I"]
 
+option= int(input("Brasil (1), senão (2): ")
+if option == 2:
+   parte_1 = ["ei", "bi", "ci"]
+   parte_2 = ["di","i", "efi"]
+   parte_3 = ["gi", "8", "ai"]
+
 print(random.choice(parte_1),random.choice(parte_2),random.choice(parte_3))
