fa573f97c1496ecfc48f47166d2809c32658a35b
Json <jailson_junior@usp.br>
2026-03-27 11:40:57 -0300

    revolucionário

--- lerolero.py
+++ lerolero.py
@@ -1,7 +1,7 @@
 import random 
 
-parte_1 = ["", "", ""]
-parte_2 = ["", "", ""]
-parte_3 = ["", "", ""]
+parte_1 = ["A", "B", "C"]
+parte_2 = ["D", "E", "F"]
+parte_3 = ["G", "H", "I"]
 
 print(random.choice(parte_1),random.choice(parte_2),random.choice(parte_3))
